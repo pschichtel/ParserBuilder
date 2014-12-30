689f8e392e2d8da8c41e9bae1d5698dc69495c64
Phillip Schichtel <phillip.public@schich.tel>
2014-12-30 12:17:40 +0100

    some random changes

--- src/main/java/de/cubeisland/engine/parser/ParseState.java
+++ src/main/java/de/cubeisland/engine/parser/ParseState.java
@@ -3,11 +3,19 @@ package de.cubeisland.engine.parser;
 import java.util.ArrayList;
 import java.util.List;
 
-public class State
+public class ParseState
 {
+    private static volatile short idCounter = 0;
+
+    private final short id;
+
+    {
+        this.id = idCounter++;
+    }
+
     private final List<MarkedRule> rules;
 
-    public State(List<MarkedRule> rules)
+    public ParseState(List<MarkedRule> rules)
     {
         this.rules = new ArrayList<MarkedRule>(rules);
     }
@@ -15,5 +23,10 @@ public class State
     public List<MarkedRule> getRules()
     {
         return rules;
+    }
+
+    public short getId()
+    {
+        return this.id;
     }
 }
--- src/main/java/de/cubeisland/engine/parser/RuleElement.java
+++ src/main/java/de/cubeisland/engine/parser/RuleElement.java
@@ -2,4 +2,16 @@ package de.cubeisland.engine.parser;
 
 public class RuleElement
 {
+    private static volatile short idCounter = 0;
+
+    private final short id;
+
+    {
+        this.id = idCounter++;
+    }
+
+    public short getId()
+    {
+        return this.id;
+    }
 }
