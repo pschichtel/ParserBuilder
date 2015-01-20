7a409a4d7e90aaf1a2beca4141f027e4ecf73573
Phillip Schichtel <phillip.git@schich.tel>
2015-01-20 18:02:54 -0500

    removed code duplication in the transition code, optimized State.equals and added license headers

--- src/main/java/de/cubeisland/engine/parser/rule/token/automate/SpontaneousTransition.java
+++ src/main/java/de/cubeisland/engine/parser/rule/token/automate/SpontaneousTransition.java
@@ -1,62 +1,34 @@
+/**
+ * The MIT License
+ * Copyright (c) 2014 Cube Island
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package de.cubeisland.engine.parser.rule.token.automate;
 
 import static de.cubeisland.engine.parser.rule.token.Epsilon.EPSILON;
 
-public class SpontaneousTransition implements Transition
+public class SpontaneousTransition extends Transition
 {
-    private final State origin;
-    private final State destination;
-
     public SpontaneousTransition(State origin, State destination)
     {
-        this.origin = origin;
-        this.destination = destination;
-    }
-
-    @Override
-    public State getOrigin()
-    {
-        return this.origin;
-    }
-
-    @Override
-    public State getDestination()
-    {
-        return this.destination;
-    }
-
-    @Override
-    public boolean equals(Object o)
-    {
-        if (this == o)
-        {
-            return true;
-        }
-        if (!(o instanceof SpontaneousTransition))
-        {
-            return false;
-        }
-
-        SpontaneousTransition that = (SpontaneousTransition) o;
-
-        if (!destination.equals(that.destination))
-        {
-            return false;
-        }
-        if (!origin.equals(that.origin))
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public int hashCode()
-    {
-        int result = origin.hashCode();
-        result = 31 * result + destination.hashCode();
-        return result;
+        super(origin, destination);
     }
 
     @Override
